7b3eff917182c4ab0018ba826d5da29a7a551618
Mubeen Ahmad <mubeenahmad6070@gmail.com>
2022-10-14 12:31:55 +0500

    created with basic connection

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageReceive, setMessageReceive] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceive(data.message);
+    });
+  }, []);
+
+  return (
+    <div>
+      <input placeholder="room..." onChange={(e) => setRoom(e.target.value)} />
+      <button onClick={joinRoom}>Join Room</button> <br />
+      <input
+        placeholder="messsage..."
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h2>Message: {messageReceive}</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.join("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    //just to receive_message and show it to frontend
+    // socket.broadcast.emit("receive_message", data);
+
+    //to check the chat room and show it to frontend
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server Start");
+});
