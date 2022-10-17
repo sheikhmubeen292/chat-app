a5748a9042e889086b4180da0df882e554e22958
Mubeen Ahmad <mubeenahmad6070@gmail.com>
2022-10-17 10:47:47 +0500

    changes

--- server/index.js
+++ server/index.js
@@ -1,36 +1,54 @@
+import "dotenv/config";
+
 import express from "express";
 import http from "http";
+import mongoose from "mongoose";
 import { Server } from "socket.io";
 import cors from "cors";
 
-const app = express();
-app.use(cors());
+const { MONGO_URI } = process.env;
 
-const server = http.createServer(app);
+async function main() {
+  const app = express();
+  app.use(cors());
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(socket.id, "hello");
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
+  mongoose.connect(MONGO_URI, (err, conn) => {
+    console.log(err ?? "connected with mongo atlas");
   });
 
-  socket.on("send_message", (data) => {
-    //just to receive_message and show it to frontend
-    // socket.broadcast.emit("receive_message", data);
+  const server = http.createServer(app);
 
-    //to check the chat room and show it to frontend
-    socket.to(data.room).emit("receive_message", data);
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
   });
+
+  io.on("connection", (socket) => {
+    console.log(socket.id, "hello");
+
+    socket.on("join_room", (data) => {
+      socket.join(data);
+    });
+
+    socket.on("send_message", (data) => {
+      //to check the chat room and show it to frontend
+      socket.to(data.room).emit("receive_message", data);
+    });
+  });
+
+  return server;
+}
+
+main().then((app) => {
+  const PORT = process.env.PORT || 5050;
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 });
 
-server.listen(5050, () => {
-  console.log("Server Start");
+process.on("uncaughtException", (err, origin) => {
+  fs.writeSync(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  );
 });
